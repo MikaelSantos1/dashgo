a5198deb1cfe935c2062fe45fbd0850e969a087c
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-04-15 16:38:00 -0300

    finished sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Box, Stack,Text,Link,Icon } from "@chakra-ui/react";
-import { RiDashboardLine } from "react-icons/ri";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar(){
     return(
@@ -9,10 +9,34 @@ export function Sidebar(){
                 <Box >
                     <Text fontWeight='bold' color="gray.400" fontSize='small'>GERAL</Text>
                     <Stack spacing="4" mt="8" align='stretch'>
-                        <Link display='flex' alignItems="center" color="pink.400" py="1">
+                       
+
+                        <Link display='flex' alignItems="center" >
                         <Icon as={RiDashboardLine} fontSize="20"/>
                         <Text ml="4" fontWeight='medium'>Dashbord</Text>
                         </Link>
+
+                        <Link display='flex' alignItems="center" >
+                        <Icon as={RiContactsLine} fontSize="20"/>
+                        <Text ml="4" fontWeight='medium'>Usuarios</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+
+                <Box >
+                    <Text fontWeight='bold' color="gray.400" fontSize='small'>AUTOMAÇÃO</Text>
+                    <Stack spacing="4" mt="8" align='stretch'>
+                        <Link display='flex' alignItems="center" >
+                        <Icon as={RiInputMethodLine} fontSize="20"/>
+                        <Text ml="4" fontWeight='medium'>Formularios</Text>
+                        </Link>
+
+                        <Link display='flex' alignItems="center" >
+                        <Icon as={RiGitMergeLine} fontSize="20"/>
+                        <Text ml="4" fontWeight='medium'>Automoção</Text>
+                        </Link>
+
+                     
                     </Stack>
                 </Box>
            </Stack>
